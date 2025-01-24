d2745c680477315313279214993a5c194bc48454
edwin garcia <garciaramiro000@gmail.com>
2025-01-23 22:11:42 -0400

    agrega el responsive

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import { labels } from "../../constants/labels";
 import { navMenuItems } from "../../constants/staticList";
+import IconSelect from "../IconSelect/iconSelect";
 
 export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <div className="fixed w-full z-50">
-      <div className="bg-primary text-white flex  justify-between items-center h-32 pe-80 ps-5 py-5">
+    <div className="fixed w-full z-50" >
+      <div className={"bg-primary text-white flex justify-between items-center md:h-32 md:pe-80 ps-5 py-5" }  >
         <div className="h-full flex items-center">
           <a
             href=""
@@ -12,7 +16,7 @@ export default function Header() {
           ></a>
           <h2 className="font-mono text-2xl ms-2">{labels.pageName}</h2>
         </div>
-        <div className=" text-quaternary bg-primary h-full flex justify-center items-center">
+        <div className=" hidden md:visible md:text-quaternary md:bg-primary md:h-full md:flex md:justify-center md:items-center">
           {navMenuItems.map((item, index) => {
             return (
               <a
@@ -24,6 +28,29 @@ export default function Header() {
               </a>
             );
           })}
+        </div>
+        <span
+          onClick={() => setMenuOpen(!menuOpen)}
+          className="md:hidden  me-10 hover:text-tertiary transition-all duration-300 ease-out"
+        >
+          <IconSelect icon={menuOpen ? "close" : "menu"} />
+        </span>
+        <div className={ menuOpen ? "md:hidden absolute top-32 right-0 bg-primary text-white w-60 h-96 flex flex-col items-center justify-center" : "hidden"}>
+          {menuOpen && (
+            <div className="absolute top-32 right-0 bg-primary text-white w-60 h-96 flex flex-col items-center justify-center">
+              {navMenuItems.map((item, index) => {
+                return (
+                  <a
+                    className="hover:text-tertiary my-4"
+                    key={index}
+                    href={item.link}
+                  >
+                    {item.label}
+                  </a>
+                );
+              })}
+            </div>
+          )}
         </div>
       </div>
     </div>
