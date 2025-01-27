c3df301c54c8c0965e816587fae42dcedb796489
edwin garcia <garciaramiro000@gmail.com>
2025-01-27 17:06:41 -0400

    agrega seccion de proyectos responsive

--- src/components/sections/recentProjects/recentProjects.tsx
+++ src/components/sections/recentProjects/recentProjects.tsx
@@ -17,10 +17,37 @@ export default function RecentProjectsSection() {
       <div className="flex justify-center">
         <div className="mt-3">{t("recentProjects.title")}</div>
       </div>
-      <div className="mt-10">
+      <div className="mt-10 hidden md:block">
         <Carousel
           centerMode={true}
-          centerPadding="100px"
+          centerPadding="100px "
+          slidesToShow={3}
+          autoplay={true}
+          autoplaySpeed={3000}
+          arrows={true}
+          draggable={true}
+        >
+          {projects.map((project, index) => (
+            <div
+              key={index}
+              className=" p-5 cursor-pointer  hover:scale-110 transform transition duration-500 ease-in-out"
+            >
+              <div className=" bg-tertiary/50 rounded-lg flex justify-center items-center">
+                <img src={project.image} />
+                <div className=" w-3 h-3 bg-tertiary"></div>
+              </div>
+
+              <p className="text-center text-quaternary mt-3">
+                {project.title}
+              </p>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+      <div className="mt-10 md:hidden block">
+        <Carousel
+          centerMode={true}
+          centerPadding="0px "
           slidesToShow={3}
           autoplay={true}
           autoplaySpeed={3000}
