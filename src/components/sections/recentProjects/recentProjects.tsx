cf739d5f5d101ab8492c157367be2413a8344988
edwin garcia <garciaramiro000@gmail.com>
2025-01-27 16:54:38 -0400

    agrega slider de proyectos

--- src/components/sections/recentProjects/recentProjects.tsx
+++ src/components/sections/recentProjects/recentProjects.tsx
@@ -1,31 +1,49 @@
 import { Carousel } from "antd";
 import { projects } from "../../../constants/staticList";
+import { useTranslation } from "react-i18next";
 
 export default function RecentProjectsSection() {
-  const contentStyle: React.CSSProperties = {
-    height: "160px",
-    color: "#fff",
-    lineHeight: "160px",
-    textAlign: "center",
-  };
+  const [t] = useTranslation("global");
   return (
     <article className=" px-12 md:px-24 py-24 bg-primary">
       <div className=" flex justify-center">
-        <div className="text-quaternary me-3 text-4xl">Proyectos</div>
-        <div className="text-tertiary text-4xl">Recientes</div>
+        <div className="text-quaternary me-3 text-4xl">
+          {t("recentProjects.recent")}
+        </div>
+        <div className="text-tertiary text-4xl">
+          {t("recentProjects.projects")}
+        </div>
       </div>
-      <Carousel autoplay arrows={true} adaptiveHeight={true} draggable={true}>
-        {projects.map((project, index) => (
-          <div key={index}>
-            <img
-            className="object-cover w-full h-96"
-              src={project.image}
-              alt={project.title}
-              style={contentStyle}
-            />
-          </div>
-        ))}
-      </Carousel>
+      <div className="flex justify-center">
+        <div className="mt-3">{t("recentProjects.title")}</div>
+      </div>
+      <div className="mt-10">
+        <Carousel
+          centerMode={true}
+          centerPadding="100px"
+          slidesToShow={3}
+          autoplay={true}
+          autoplaySpeed={3000}
+          arrows={true}
+          draggable={true}
+        >
+          {projects.map((project, index) => (
+            <div
+              key={index}
+              className=" p-5 cursor-pointer  hover:scale-110 transform transition duration-500 ease-in-out"
+            >
+              <div className=" bg-tertiary/50 rounded-lg flex justify-center items-center">
+                <img src={project.image} />
+                <div className=" w-3 h-3 bg-tertiary"></div>
+              </div>
+
+              <p className="text-center text-quaternary mt-3">
+                {project.title}
+              </p>
+            </div>
+          ))}
+        </Carousel>
+      </div>
     </article>
   );
 }
