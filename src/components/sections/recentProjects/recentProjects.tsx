40cd4b12328c4d1df730f45d3a36b3cc80eee1a1
Edwin Ramiro Garcia Chambilla <edwin.garcia@et.bo>
2025-02-06 23:38:46 -0400

    agrega fixeos al responsive

--- src/components/sections/recentProjects/recentProjects.tsx
+++ src/components/sections/recentProjects/recentProjects.tsx
@@ -6,11 +6,11 @@ export default function RecentProjectsSection() {
   const [t] = useTranslation("global");
   return (
     <article className=" px-12 md:px-24 py-24 bg-primary" id="projects">
-      <div className=" md:flex justify-center">
-        <div className="text-quaternary me-3 text-4xl">
+      <div className=" flex justify-center">
+        <div className="text-quaternary me-3 md:text-4xl text-3xl ">
           {t("recentProjects.recent")}
         </div>
-        <div className="text-tertiary text-4xl">
+        <div className="text-tertiary md:text-4xl text-3xl">
           {t("recentProjects.projects")}
         </div>
       </div>
@@ -26,7 +26,6 @@ export default function RecentProjectsSection() {
           autoplaySpeed={3000}
           arrows={true}
           draggable={true}
-          
         >
           {projects.map((project, index) => (
             <div
@@ -34,13 +33,20 @@ export default function RecentProjectsSection() {
               className=" p-5 cursor-pointer  hover:scale-110 transform transition duration-500 ease-in-out"
             >
               <div className="  rounded-lg flex justify-center items-center">
-                <img src={project.image}
+                <img
+                  src={project.image}
                   className=" h-60 object-cover rounded-lg  "
                 />
               </div>
-
-              <p className="text-center text-quaternary mt-3">
+              <p
+                className="text-center absolute h-24 
+                bottom-2 bg-primary/60 w-full text-quaternary font-semibold
+                flex pt-2 items-center flex-col text-lg "
+              >
                 {project.title}
+                <p
+                  className="text-center mt-2 me-10 px-4 text-quaternary text-sm font-normal"
+                >{project.description}</p>
               </p>
             </div>
           ))}
@@ -50,7 +56,7 @@ export default function RecentProjectsSection() {
         <Carousel
           centerMode={true}
           centerPadding="0px "
-          slidesToShow={3}
+          slidesToShow={1}
           autoplay={true}
           autoplaySpeed={3000}
           arrows={false}
@@ -61,13 +67,18 @@ export default function RecentProjectsSection() {
               key={index}
               className=" p-2 mb-6 cursor-pointer  hover:scale-110 transform transition duration-500 ease-in-out"
             >
-              <div className=" bg-tertiary/50 rounded-lg flex justify-center items-center">
-                <img src={project.image} 
-                  className="w-64 h-64 object-cover rounded-lg  "
+              <div className=" relative rounded-lg flex justify-center items-center">
+                <img
+                  src={project.image}
+                  className="object-cover rounded-lg w-full  "
                 />
               </div>
 
-              <p className="text-center text-quaternary mt-3">
+              <p
+                className="text-center absolute h-14 
+                bottom-2 bg-primary/60 w-full text-quaternary font-semibold
+                flex justify-center items-center "
+              >
                 {project.title}
               </p>
             </div>
